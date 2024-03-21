3eb2467251ef6a7337ab66634354baab7f10aad0
melinaskye <melinaskye@optonline.net>
2024-03-21 15:45:53 -0400

    fixed

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
 
 
 // Configure Nunjucks
